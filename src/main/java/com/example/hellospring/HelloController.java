2eae5379fa7c1c4646def27f48cd4cd78778ef44
june0 <june0.oh.00@gmail.com>
2022-09-16 15:04:08 +0900

    응답 메시지 수정

--- src/main/java/com/example/hellospring/HelloController.java
+++ src/main/java/com/example/hellospring/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "SpringBoot :D");
+        model.addAttribute("data", "SpringBoot :)");
         return "hello";
     }
 }
